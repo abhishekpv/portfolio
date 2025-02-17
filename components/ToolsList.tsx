bec0fda3eb517d846a6caf575a82687521d457ed
Abhishek P V <abhishek.pottammal@qburst.com>
2025-02-17 17:25:11 +0530

    increase animation speed

--- components/ToolsList.tsx
+++ components/ToolsList.tsx
@@ -30,7 +30,7 @@ const ToolCard = ({
         alt={title}
       />
       <p
-        className={`w-full font-Outfit h-full absolute text-gray-700 flex items-center duration-1000 bg-transparent backdrop-blur-sm  justify-center top-0 ${
+        className={`w-full font-Outfit h-full absolute text-gray-700 flex items-center duration-700 bg-transparent backdrop-blur-sm  justify-center top-0 ${
           isHover ? "opacity-100 scale-100" : "opacity-0 scale-150"
         }`}
       >
