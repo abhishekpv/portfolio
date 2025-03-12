d9e0197a8ce27fdc43a97b79b1420b92d4a99865
Abhishek P V <abhishek.pottammal@qburst.com>
2025-03-12 18:18:04 +0530

    dark fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="scroll-smooth dark">
+    <html lang="en" className="scroll-smooth">
       <body
         className={`min-h-screen relative ${outfit.className} ${ovo.className} antialiased leading-8 overflow-x-hidden dark:bg-darkTheme`}
       >
