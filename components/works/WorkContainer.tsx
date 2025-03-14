5a32f957297ca7cef8b6539cab62f8984a6b21db
Abhishek P V <abhishek.pottammal@qburst.com>
2025-03-14 12:18:29 +0530

    change works UI

--- components/works/WorkContainer.tsx
+++ components/works/WorkContainer.tsx
@@ -3,27 +3,35 @@ import Image from "next/image";
 
 const WorkContainer = () => {
   return (
-    <div className="flex flex-wrap items-center justify-center max-w-7xl mx-auto w-fit gap-5">
+    <div className="grid xl:grid-cols-3 md:grid-cols-2 grid-cols-1 max-w-7xl mx-auto gap-5">
       {workData.map((project) => {
         return (
           <div
-            className="aspect-square bg-no-repeat w-72 bg-cover border bg-center rounded-lg relative cursor-pointer group"
+            className=" bg-white mx-auto max-w-96  w-full overflow-hidden relative group border-[0.5px] border-gray-200 shadow-sm rounded-lg "
             key={project.id}
-            style={{ backgroundImage: `url(${project.bgImage})` }}
           >
-            <div className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
-              <div>
-                <h2 className="font-semibold text-black">{project.title}</h2>
-                <p className="text-sm text-gray-700">{project.description}</p>
-              </div>
-              <div className="border rounded-full duration-300 border-black w-9 aspect-square flex items-center justify-center shadow-[2px_2px_0_#000] group-hover:bg-lime-300 transition">
-                <Image
-                  src={assets.send_icon}
-                  alt={"send icon"}
-                  className="w-5"
-                />
-              </div>
+            <div className="h-52  overflow-hidden border-b-[0.5px] border-gray-200 ">
+              <Image
+                src={project.bgImage}
+                alt={project.title}
+                layout="responsive"
+                width="300"
+                height="200"
+                className=" min-h-52 min-w-72 sm:min-w-96 -mt-1"
+              />
             </div>
+            <div className="px-3 py-1  flex items-center justify-between w-full">
+              <h3 className="text-lg font-semibold duration-200 text-black flex items-center  gap-2">
+                {project.title}<Image src={assets.open_link_black} alt="external-link" className="h-3 w-3 min-w-3 cursor-pointer" />
+              </h3>
+              <p className="text-gray-500 text-xs">{project.description}</p>
+            </div>
+            <p className="px-3 text-sm text-justify text-gray-800 py-2">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem
+              corporis ullam ratione corrupti officia soluta, deserunt enim
+              impedit cumque, tenetur fugit magni ipsa nemo? Cum nihil accusamus
+              nisi corrupti quisquam!
+            </p>
           </div>
         );
       })}
