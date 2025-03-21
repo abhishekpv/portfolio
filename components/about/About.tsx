b525087a024f75beaa1a8e115d25a168f7df2508
Abhishek P V <pvabhishek26@gmail.com>
2025-03-21 15:52:20 +0530

    adjust the animation delay for abour

--- components/about/About.tsx
+++ components/about/About.tsx
@@ -41,7 +41,7 @@ const About = () => {
         <motion.div
           initial={{ opacity: 0 }}
           whileInView={{ opacity: 1 }}
-          transition={{ duration: 0.6, delay: 0.8 }}
+          transition={{ duration: 0.6 }}
           className="fle"
         >
           <p
@@ -57,7 +57,7 @@ const About = () => {
           <motion.ul
             initial={{ opacity: 0 }}
             whileInView={{ opacity: 1 }}
-            transition={{ duration: 0.8, delay: 0.5 }}
+            transition={{ duration: 0.8 }}
             className="flex gap-6 max-xl:flex-wrap max-w-3xl "
           >
             <TechStack />
@@ -68,7 +68,7 @@ const About = () => {
             onMouseLeave={() => setCursorType("default")}
             initial={{ opacity: 0, y: 20 }}
             whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5, delay: 0.3 }}
+            transition={{ duration: 0.5 }}
             className="mt-6 mb-3 text-gray-700 font-Ovo"
           >
             Tools I use
